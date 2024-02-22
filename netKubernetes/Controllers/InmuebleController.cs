3db93d03715f355c4fbbd72f1360eaf214921573
gioh <gioh2020@gmail.com>
2024-02-21 22:25:42 -0500

    devGio/se agregó el controlador  de inmueble y usuario

---
+++ netKubernetes/Controllers/InmuebleController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using netKubernetes.Data.Inmuebles;
+using AutoMapper;
+using netKubernetes.Dto.InmuebleDto;
+using netKubernetes.Middleware;
+using System.Net;
+using netKubernetes.Models;
+
+namespace netKubernetes.Controllers
+{
+    public class InmuebleController : ControllerBase
+    {
+        private readonly IInmubleRepository _repository;
+        private IMapper _mapper;
+        public InmuebleController(
+            IInmubleRepository repository
+            , IMapper mapper
+        )
+        {
+            _repository = repository;
+            _mapper = mapper;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<InmuebleResponseDto>>> getInmuebles()
+        {
+            var inmuebles = await _repository.GetAllInmuebles();
+            return Ok(_mapper.Map<IEnumerable<InmuebleResponseDto>>(inmuebles));
+        }
+
+        [HttpGet("{id}", Name = "GetInmuebleById")]
+        public async Task<ActionResult<IEnumerable<InmuebleResponseDto>>> getInmuebleById(int id)
+        {
+            var inmueble = await _repository.GetInmuebleById(id);
+
+            if (inmueble is null)
+            {
+                throw new MiddlewareException(
+                  HttpStatusCode.NotFound,
+                  new { mensaje = $"No se encontro el inmuble con ese id" }
+                );
+            }
+
+            return Ok(_mapper.Map<IEnumerable<InmuebleResponseDto>>(inmueble));
+        }
+        [HttpPost]
+        public async Task<ActionResult<InmuebleResponseDto>> CreateInmueble([FromBody] InmuebleRequestDto inmueble)
+        {
+            var inmubleModel =  _mapper.Map<Inmueble>(inmueble);
+           await _repository.CreateInmueble(inmubleModel);
+           await _repository.SaveChanges();
+
+            var inmuebleResponse = _mapper.Map<InmuebleRequestDto>(inmubleModel);
+
+            return CreatedAtRoute(nameof(getInmuebleById), new { inmuebleResponse.Id }, inmuebleResponse);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteInmueble(int id)
+        { 
+            await _repository.DeleteInmueble(id);
+            await _repository.SaveChanges();
+            return Ok();
+        }
+
+
+
+    }
+}
---
+++ netKubernetes/Controllers/UsuarioController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using netKubernetes.Data.Usuarios;
+using netKubernetes.Dto.UsuarioDto;
+
+namespace netKubernetes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository _repository;
+        public UsuarioController(IUsuarioRepository suarioRepository)
+        {
+            _repository = suarioRepository;
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<UsuarioResponseDto>> Login([FromBody] UsuarioLoginRequestDto request)
+        {
+            return await _repository.Login(request);
+
+        }
+
+        [AllowAnonymous]
+        [HttpPost("registrar")]
+        public async Task<ActionResult<UsuarioResponseDto>> registrar([FromBody] UsuarioRegistroRequestDtocs request)
+        {
+            return await _repository.RegistrarUsuario(request);
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<UsuarioResponseDto>> DevolverUsuario([FromBody] UsuarioLoginRequestDto request)
+        {
+            return await _repository.GetUsuario();
+
+        }
+    }
+}
