d23c425e2c1d25ffcf78b5818f86e73c728bd689
gioh <gioh2020@gmail.com>
2024-02-21 22:26:36 -0500

    devGio/se agregó los dtos de inmueble y usuario

---
+++ netKubernetes/Profiles/InmuebleProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using netKubernetes.Dto.InmuebleDto;
+using netKubernetes.Models;
+
+namespace netKubernetes.Profiles
+{
+    public class InmuebleProfile : Profile
+    {
+        public InmuebleProfile()
+        {
+             CreateMap<Inmueble, InmuebleResponseDto>();
+             CreateMap<InmuebleRequestDto, Inmueble>();
+        }
+
+    }
+}
