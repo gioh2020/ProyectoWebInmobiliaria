3f70d5c864c18299542fdacdb152d10c716511c0
gioh <gioh2020@gmail.com>
2024-02-21 22:21:41 -0500

    devGio/se agrego el middleware

---
+++ netKubernetes/Middleware/MiddlewareException.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace netKubernetes.Middleware
+{
+    public class MiddlewareException: Exception
+    {
+
+        public HttpStatusCode Codigo { get; set; }  
+        public Object? Errores { get; set;}
+        public MiddlewareException(HttpStatusCode codigo, object? errores = null) { 
+        
+            Codigo = codigo;    
+            Errores = errores;
+        
+        }
+
+    }
+}
