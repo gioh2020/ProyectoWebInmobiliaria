01fba51d5ea0d19c3fdca641310207d491ff8d8a
gioh <gioh2020@gmail.com>
2024-02-21 22:22:50 -0500

    devGio/se agregó las interfaces de usuario e inmueble

---
+++ netKubernetes/Data/Inmuebles/IInmubleRepository.cs
@@ -0,0 +1,13 @@
+﻿using netKubernetes.Models;
+
+namespace netKubernetes.Data.Inmuebles
+{
+    public interface IInmubleRepository
+    {
+        Task<bool> SaveChanges();
+        Task<IEnumerable<Inmueble>> GetAllInmuebles();
+        Task<Inmueble> GetInmuebleById(int id);
+        Task CreateInmueble(Inmueble inmueble);
+        Task DeleteInmueble(int id);
+    }
+}
