7c1ce456bcea0f3c4c4025293a9c43fcb03b6018
gioh <gioh2020@gmail.com>
2024-02-21 22:23:48 -0500

    devGio/se agregó los dtos de inmueble y usuario

---
+++ netKubernetes/Dto/InmuebleDto/InmuebleResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace netKubernetes.Dto.InmuebleDto
+{
+    public class InmuebleResponseDto
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Direccion { get; set; }
+        public string? Precio { get; set; }
+        public string? Picture { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+
+    }
+}
