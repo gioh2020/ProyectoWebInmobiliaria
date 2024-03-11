d50387e372800c1fb4200ab0dd4beed772e3c274
gioh <gioh2020@gmail.com>
2024-03-10 22:23:25 -0500

    devGio/configuracion general

--- netKubernetes/obj/Release/net8.0/netKubernetes.AssemblyInfo.cs
+++ netKubernetes/obj/Release/net8.0/netKubernetes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("netKubernetes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d7d979a6ae75b77c4baf28617b481d57ba62203")]
 [assembly: System.Reflection.AssemblyProductAttribute("netKubernetes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("netKubernetes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
