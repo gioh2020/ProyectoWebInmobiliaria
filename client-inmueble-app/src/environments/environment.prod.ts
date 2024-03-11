62bc9cb9a14506cb252d2294151bb72e75006d19
gioh <gioh2020@gmail.com>
2024-03-10 22:18:42 -0500

    devGio/se crea y se configura el inicio del front

---
+++ client-inmueble-app/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  name: 'prod',
+  firebase: {
+    config: {
+      apiKey: "AIzaSyBV9Wl4bIQXb3i-T6wzIPwT-GfyJsjglf0",
+      authDomain: "edificaion-app.firebaseapp.com",
+      projectId: "edificaion-app",
+      storageBucket: "edificaion-app.appspot.com",
+      messagingSenderId: "1028797408734",
+      appId: "1:1028797408734:web:457604d67c23c4666ea94f"
+    }
+  }
+};
