2ecdbd7a841c07aec7fa8ada55f8c467297fd8d5
gioh <gioh2020@gmail.com>
2024-04-22 22:04:31 -0500

    devGioh/se implemeta la carga de archivos

--- client-inmueble-app/src/app/shared/popups/files-upload/files-upload.component.ts
+++ client-inmueble-app/src/app/shared/popups/files-upload/files-upload.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+export interface DialogData{
+  multiple: boolean,
+  crop: boolean
+}
 @Component({
   selector: 'app-files-upload',
   templateUrl: './files-upload.component.html',
@@ -7,13 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilesUploadComponent implements OnInit {
 
-  constructor() { }
+  isHovering!: boolean
+  files: File[] = [];
+  imageFile!: File;
+  isError!: boolean;
+
+  filesUrls: string[] =[]
+
+
+  constructor(private diaLogRef: MatDialogRef<FilesUploadComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData) { 
+
+
+  }
 
   ngOnInit(): void {
   }
-  isHovering!: boolean
+
   toggleHover(event:boolean){
     this.isHovering = event
+  }
+
+  onDrop(files: FileList): void {
+    this.dropGeneral(files)
+  }
+  onDropFile(event: FileList | any): void {
+    console.log(event.target.files);
+    
+    this.dropGeneral(event.target.files)
+  }
+
+  dropGeneral(files: FileList | any): void {
+    this.isError = false
+    if (this.data.crop && files.legth>1) {
+      this.isError = true;
+      return;
+      
+    }
+    for (let i = 0; i < files.length; i++) {
+      this.files.push(files.item(i) as File)
+      
+    }
+    console.log(files)
+  }
+  cli(){
+    console.log('func');
+    
   }
 
 }
