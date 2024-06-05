b4e81e2fb56191ff710f95d7196eec6ae8ceb3fe
gioh <gioh2020@gmail.com>
2024-06-04 21:28:05 -0500

    devGioh/se crea el componente login

---
+++ client-inmueble-app/src/app/pages/auth/pages/login/login.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatInputModule} from '@angular/material/input';
+
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatIconModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatInputModule
+    
+  ]
+})
+export class LoginModule { }
